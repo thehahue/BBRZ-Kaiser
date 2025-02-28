942aed3cf82e0e7459f2130c2057d80a6ba9e32e
ahwz <andreas.zincke@gmx.at>
2025-02-28 08:33:43 +0100

    initialize spring boot

---
+++ src/test/java/at/bbrz/kaiser/KaiserApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bbrz.kaiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
