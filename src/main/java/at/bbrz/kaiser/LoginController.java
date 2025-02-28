58ec0f4434872d654c9759d4432a01e111c61dd7
ahwz <andreas.zincke@gmx.at>
2025-02-28 10:58:01 +0100

    remove unnecessary imports and make attribute final in LoginController

--- src/main/java/at/bbrz/kaiser/LoginController.java
+++ src/main/java/at/bbrz/kaiser/LoginController.java
@@ -1,17 +1,15 @@
 package at.bbrz.kaiser;
 
 import at.bbrz.kaiser.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
 
 @RestController
 public class LoginController {
 
-    private LoginService loginService = new LoginService();
+    private final LoginService loginService = new LoginService();
 
 
     @GetMapping("/login?name={name}&pw={password}")
