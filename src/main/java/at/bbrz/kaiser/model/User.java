80af981efbcd3c5bbcece89e618ea828694e947c
David Windisch <zwilling_dave@outlook.com>
2025-03-03 13:06:25 +0100

    added some Annotations

--- src/main/java/at/bbrz/kaiser/model/User.java
+++ src/main/java/at/bbrz/kaiser/model/User.java
@@ -1,10 +1,14 @@
 package at.bbrz.kaiser.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Builder
+@Table(name = "users") // Changed Table-Name because H2 has already a "user" table
 public class User {
     private String name;
     private String password;
