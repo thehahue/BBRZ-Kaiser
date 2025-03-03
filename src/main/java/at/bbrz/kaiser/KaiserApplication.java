64389dfdfb707ba3fe6312606755c3b705ced2c9
David Windisch <zwilling_dave@outlook.com>
2025-03-03 13:11:02 +0100

    autowired userRepository

--- src/main/java/at/bbrz/kaiser/KaiserApplication.java
+++ src/main/java/at/bbrz/kaiser/KaiserApplication.java
@@ -1,10 +1,20 @@
 package at.bbrz.kaiser;
 
+import at.bbrz.kaiser.model.User;
+import at.bbrz.kaiser.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.List;
 
 @SpringBootApplication
 public class KaiserApplication {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KaiserApplication.class, args);
