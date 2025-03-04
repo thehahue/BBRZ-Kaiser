1ee0c68d136a82e0c8812cd6b8683987ac3a4dca
David Windisch <zwilling_dave@outlook.com>
2025-03-04 09:24:32 +0100

    added 2parameter(name, password) constructor for User

--- src/main/java/at/bbrz/kaiser/model/User.java
+++ src/main/java/at/bbrz/kaiser/model/User.java
@@ -15,4 +15,9 @@ public class User {
     private Integer id;
     private String name;
     private String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
 }
