18e1627526125fbf80dcd65823770ae3d9985fdc
David Windisch <zwilling_dave@outlook.com>
2025-03-07 08:59:32 +0100

    added run profile (development) to database init

--- src/main/java/at/bbrz/kaiser/KaiserApplication.java
+++ src/main/java/at/bbrz/kaiser/KaiserApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 
 import java.util.List;
@@ -20,7 +21,7 @@ public class KaiserApplication {
 		SpringApplication.run(KaiserApplication.class, args);
 
 	}
-
+	@Profile("development")
 	@EventListener(ApplicationReadyEvent.class)
 	private void initDatabaseData(){
 		userRepository.saveAll(List.of(
