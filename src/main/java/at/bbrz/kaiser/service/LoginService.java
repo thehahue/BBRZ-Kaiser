33572e59ee79831a7366bfd3e96714874949fd42
David Windisch <zwilling_dave@outlook.com>
2025-03-07 09:32:20 +0100

    added userrepo to loginservice

--- src/main/java/at/bbrz/kaiser/service/LoginService.java
+++ src/main/java/at/bbrz/kaiser/service/LoginService.java
@@ -1,16 +1,20 @@
 package at.bbrz.kaiser.service;
 
+import at.bbrz.kaiser.model.User;
+import at.bbrz.kaiser.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 public class LoginService {
-    private final Map<String, String> loginData = Map.of(
-            "dave", "pass"
-    );
-    public boolean couldLoginWith(String user, String password) {
-        if (user == null || password == null) {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean couldLoginWith(String username, String password) {
+        if (username == null || password == null) {
             return false;
         }
         return loginData.containsKey(user) && loginData.get(user).equals(password);
