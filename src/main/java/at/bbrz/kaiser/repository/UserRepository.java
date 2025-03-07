0c96e8a375c9e890242910e3eaa4b47715fac921
David Windisch <zwilling_dave@outlook.com>
2025-03-07 11:30:58 +0100

    added findByNameAndPassword method

--- src/main/java/at/bbrz/kaiser/repository/UserRepository.java
+++ src/main/java/at/bbrz/kaiser/repository/UserRepository.java
@@ -3,5 +3,6 @@ package at.bbrz.kaiser.repository;
 import at.bbrz.kaiser.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
+    User findByNameAndPassword(String username, String password);
 }
