64d0d0202c837b6f542882852edf38a68abec8d7
ahwz <andreas.zincke@gmx.at>
2025-03-14 12:28:19 +0100

    Add service annotation

--- src/main/java/at/bbrz/kaiser/service/JWTWrapper.java
+++ src/main/java/at/bbrz/kaiser/service/JWTWrapper.java
@@ -3,9 +3,11 @@ package at.bbrz.kaiser.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
+@Service
 public class JWTWrapper {
     public String createJWTToken(String userName, Instant expiryDate) throws JWTCreationException {
         Algorithm algorithm = Algorithm.HMAC256("Kaiser");
