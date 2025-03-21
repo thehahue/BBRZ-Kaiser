c5b6c188cbeba652d3a9eb9380d5fbaa4ff044c4
ahwz <andreas.zincke@gmx.at>
2025-03-21 12:11:34 +0100

    add /secureTest Endpoint, TODO: Remove user parameter from TokenService.validateToken, change TokenService.validateToken from return value boolean to void

--- src/main/java/at/bbrz/kaiser/controller/HelloController.java
+++ src/main/java/at/bbrz/kaiser/controller/HelloController.java
@@ -1,12 +1,41 @@
 package at.bbrz.kaiser.controller;
 
+import at.bbrz.kaiser.service.TokenService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    TokenService tokenService;
+
+    public HelloController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/secureTest")
+    public ResponseEntity<String> secureEndpoint(@RequestHeader("Authorization") String authHeader) {
+        String[] authHeaderSplit = authHeader.split(" ");
+        if (authHeaderSplit.length != 2 || !authHeaderSplit[0].equals("Bearer")) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Incorrect Header");
+        }
+        try {
+            tokenService.validateToken(authHeaderSplit[1], "user");
+            return ResponseEntity.ok("Verified");
+        } catch (JWTVerificationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not Verified");
+        }
     }
 }
---
+++ src/test/java/at/bbrz/kaiser/controller/HelloControllerTest.java
@@ -0,0 +1,83 @@
+package at.bbrz.kaiser.controller;
+
+import at.bbrz.kaiser.service.TokenService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = { TokenService.class, HelloController.class })
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+    private HelloController helloController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private TokenService tokenService;
+
+    @BeforeEach
+    void setUp() {
+        helloController = new HelloController(tokenService);
+    }
+
+    @Test
+    void secureEndpointReturnsHeaderWithValidToken() {
+        String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjE3NDE5NDI1ODN9.q7OZfffwwH_U0k2j6oI1__mTgOw2R6JxNzYTllfQK_Q";
+        ResponseEntity<String> response = helloController.secureEndpoint("Bearer " + validToken);
+        assertEquals(ResponseEntity.ok("Verified"), response);
+    }
+
+    @Test
+    void secureEndpoint_ValidToken() throws Exception {
+        String validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InVzZXIiLCJleHAiOjU3NDE5NDI1ODN9.0xkhKS8VKl-m4meKIEkd6-qXm_OZox1EkvWzNUTphLs";
+
+        Mockito.when(tokenService.validateToken(validToken, "user")).thenReturn(true);
+
+        mockMvc.perform(post("/secureTest")
+                .header("Authorization", "Bearer " + validToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Verified"));
+    }
+
+    @Test
+    void secureEndpoint_InvalidToken() throws Exception {
+        String invalidToken = "invalid";
+
+        Mockito.when(tokenService.validateToken(invalidToken, "user"))
+                .thenThrow(JWTVerificationException.class);
+
+        mockMvc.perform(post("/secureTest")
+                        .header("Authorization", "Bearer " + invalidToken))
+                .andExpect(status().isUnauthorized())
+                .andExpect(content().string("Not Verified"));
+    }
+
+    @Test
+    void secureEndpoint_MissingHeader() throws Exception{
+        mockMvc.perform(post("/secureTest"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void secureEndpoint_InvalidHeaderFormat() throws Exception{
+        mockMvc.perform(post("/secureTest")
+                .header("Authorization", "invalid"))
+                .andExpect(status().isBadRequest());
+    }
+}
