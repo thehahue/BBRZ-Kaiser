d595fe3ca339df0d94fbcf34a5d0bb28f54c402e
ahwz <andreas.zincke@gmx.at>
2025-04-04 11:36:20 +0200

    change responseentity to custom payload object instead of string

--- src/main/java/at/bbrz/kaiser/controller/HelloController.java
+++ src/main/java/at/bbrz/kaiser/controller/HelloController.java
@@ -1,15 +1,19 @@
 package at.bbrz.kaiser.controller;
 
+import at.bbrz.kaiser.model.PayloadResponse;
 import at.bbrz.kaiser.service.TokenService;
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import lombok.extern.java.Log;
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Log
 @RestController
 public class HelloController {
 
@@ -37,5 +41,17 @@ public class HelloController {
         } catch (JWTVerificationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"status\":\"not verified\"}");
         }
+    }
+
+
+    @GetMapping("/decodePayload")
+    public ResponseEntity<PayloadResponse> decodePayload(@CookieValue(value = "token") String token) {
+        DecodedJWT jwt = JWT.decode(token);
+        String username = jwt.getClaim("username").asString();
+        PayloadResponse response = PayloadResponse.builder()
+                .username(username)
+                .build();
+
+        return ResponseEntity.ok(response);
     }
 }
