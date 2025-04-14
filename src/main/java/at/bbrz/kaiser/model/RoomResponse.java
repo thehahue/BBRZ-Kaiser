61c73415794c578ec9006515d093385aa8619f0d
David Windisch <zwilling_dave@outlook.com>
2025-04-14 07:57:20 +0200

    added a RoomResponse class

---
+++ src/main/java/at/bbrz/kaiser/model/RoomResponse.java
@@ -0,0 +1,19 @@
+package at.bbrz.kaiser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class RoomResponse {
+    private String uuid;
+    private String name;
+    private List<User> users;
+    private Boolean success;
+}
