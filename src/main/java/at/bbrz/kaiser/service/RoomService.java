256ea870db2cdacb0512b25946443755a54960f2
David Windisch <zwilling_dave@outlook.com>
2025-04-14 08:00:03 +0200

    added RoomService

---
+++ src/main/java/at/bbrz/kaiser/service/RoomService.java
@@ -0,0 +1,43 @@
+package at.bbrz.kaiser.service;
+
+import at.bbrz.kaiser.exceptions.RoomNotFoundException;
+import at.bbrz.kaiser.model.Room;
+import at.bbrz.kaiser.model.User;
+import at.bbrz.kaiser.repository.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomService {
+
+    private RoomRepository roomRepository;
+
+    @Autowired
+    public RoomService(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+
+    public String findNameById(String id){
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+
+        if (optionalRoom.isEmpty()) {
+            throw new RoomNotFoundException("Room doesn't exist");
+        }
+
+        return optionalRoom.get().getName();
+    }
+
+    public List<User> findUsersById(String id) {
+        Optional<Room> optionalRoom = roomRepository.findById(id);
+
+        if (optionalRoom.isEmpty()) {
+            throw new RoomNotFoundException("Room doesn't exist");
+        }
+
+        return optionalRoom.get().getUsers();
+    }
+}
