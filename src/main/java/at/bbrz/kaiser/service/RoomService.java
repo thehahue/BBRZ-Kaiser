5642bb0c5cac8ca17db9caa96b33bfbaa13efff2
David Windisch <zwilling_dave@outlook.com>
2025-04-14 08:05:42 +0200

    changed service attribute to final

--- src/main/java/at/bbrz/kaiser/service/RoomService.java
+++ src/main/java/at/bbrz/kaiser/service/RoomService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @Service
 public class RoomService {
 
-    private RoomRepository roomRepository;
+    private final RoomRepository roomRepository;
 
     @Autowired
     public RoomService(RoomRepository roomRepository) {
