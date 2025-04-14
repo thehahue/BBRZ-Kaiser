040ebddb63a6ef50efb13b8d5b87ec2c72f79919
David Windisch <zwilling_dave@outlook.com>
2025-04-14 07:55:43 +0200

    created RoomController with /room/{roomId} GetMapping

---
+++ src/main/java/at/bbrz/kaiser/controller/RoomController.java
@@ -0,0 +1,42 @@
+package at.bbrz.kaiser.controller;
+
+import at.bbrz.kaiser.model.RoomResponse;
+import at.bbrz.kaiser.service.RoomService;
+import at.bbrz.kaiser.service.TokenService;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RoomController {
+
+    private RoomService roomService;
+    private TokenService tokenService;
+
+    @Autowired
+    public RoomController(RoomService roomService, TokenService tokenService) {
+        this.roomService = roomService;
+        this.tokenService =tokenService;
+    }
+
+    @GetMapping("/room/{roomId}")
+    public ResponseEntity<RoomResponse> room(@PathVariable String roomId, @CookieValue(value = "token") String token) {
+        try {
+            tokenService.validateToken(token);
+        } catch (JWTVerificationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(RoomResponse.builder().build());
+        }
+
+
+        return ResponseEntity.ok(RoomResponse.builder()
+                .uuid(roomId)
+                .name(roomService.findNameById(roomId))
+                .users(roomService.findUsersById(roomId))
+                .build());
+    }
+}
