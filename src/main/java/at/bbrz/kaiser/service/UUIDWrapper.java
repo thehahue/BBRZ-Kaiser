85cc22fc2765d218af1408ca304349b3ccf66bf2
ahwz <andreas.zincke@gmx.at>
2025-04-18 11:06:19 +0200

    changed UUIDWrapper to service

--- src/main/java/at/bbrz/kaiser/service/UUIDWrapper.java
+++ src/main/java/at/bbrz/kaiser/service/UUIDWrapper.java
@@ -1,7 +1,10 @@
 package at.bbrz.kaiser.service;
 
+import org.springframework.stereotype.Service;
+
 import java.util.UUID;
 
+@Service
 public class UUIDWrapper {
     public String createUUID(){
         return UUID.randomUUID().toString();
