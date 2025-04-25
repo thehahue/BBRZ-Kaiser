6451c633fc56de63abcc5f0a9b0ec54833c8dac3
ahwz <andreas.zincke@gmx.at>
2025-04-25 12:57:25 +0200

    add getter and setter to LoginResponse

--- src/main/java/at/bbrz/kaiser/model/LoginResponse.java
+++ src/main/java/at/bbrz/kaiser/model/LoginResponse.java
@@ -1,7 +1,11 @@
 package at.bbrz.kaiser.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
+@Getter
+@Setter
 @SuperBuilder
 public class LoginResponse extends AbstractResponse {
     private String token;
