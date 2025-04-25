42b032e916412e8a7d340b58acaca76becfd6eab
ahwz <andreas.zincke@gmx.at>
2025-04-25 11:39:32 +0200

    Add AbstractResponse

---
+++ src/main/java/at/bbrz/kaiser/model/AbstractResponse.java
@@ -0,0 +1,16 @@
+package at.bbrz.kaiser.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public abstract class AbstractResponse {
+    private String message;
+}
