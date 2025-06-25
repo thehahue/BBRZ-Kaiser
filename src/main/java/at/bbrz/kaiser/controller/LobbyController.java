8bb4ec209fbab5e0d40e291e98810d73f427caa4
ahwz <andreas.zincke@gmx.at>
2025-06-25 11:55:48 +0200

    implement LobbyController and RoomDto

---
+++ src/main/java/at/bbrz/kaiser/controller/LobbyController.java
@@ -0,0 +1,30 @@
+package at.bbrz.kaiser.controller;
+
+import at.bbrz.kaiser.model.RoomDto;
+import at.bbrz.kaiser.service.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class LobbyController {
+
+    @Autowired
+    private RoomService roomService;
+
+
+    @GetMapping("/lobby")
+    public String loadLobby(Model model) {
+        List<RoomDto> rooms = roomService.getAllRooms().stream()
+                .map(room -> new RoomDto(room.getName(), room.getUsers().size()))
+                .toList();
+
+        model.addAttribute("rooms", rooms);
+        return "lobby";
+    }
+
+
+}
---
+++ src/main/java/at/bbrz/kaiser/model/RoomDto.java
@@ -0,0 +1,4 @@
+package at.bbrz.kaiser.model;
+
+public record RoomDto(String name, int playerCount) {}
+
