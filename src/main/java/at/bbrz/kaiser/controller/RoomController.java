b1e2d49948f74cc9460a4f4c7f3ff8d246664e44
ahwz <andreas.zincke@gmx.at>
2025-06-28 17:09:19 +0200

    remove unused imports

--- src/main/java/at/bbrz/kaiser/controller/RoomController.java
+++ src/main/java/at/bbrz/kaiser/controller/RoomController.java
@@ -14,7 +14,6 @@ import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
